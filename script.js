fc59055dcef63967728c32b919be99173dc7296e
Courtney <courtney@scripted.org>
2017-06-21 14:39:37 +0000

    made website

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // JavaScript File
 // JavaScript File
 $( document ).ready(function() {
-
+$("h1").click(function(){
+    alert("Check out my alert!");
+});
+$('button').click(function(){
+var answer="oreo";
+if (answer===$("input").val()){
+    alert("Mmmm");
+}
+else{
+    alert("Nope, try again!")
+}
+});
 
 });
+
+
+
+
+
